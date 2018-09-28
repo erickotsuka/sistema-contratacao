4eac18fa5a41e64d72e8e4cd82ba56cec37fc97f
Lucas Magalhães <lucas.magalhaes.cordeiro@usp.br>
2018-09-28 12:24:52 -0300

    Adicao de campos de formulario na pagina publicar-demanda.js

--- src/pages/publish-demand.js
+++ src/pages/publish-demand.js
@@ -1,9 +1,82 @@
 import React from "react";
 import Layout from "../components/layout";
+import '../css/main.css';
+import '../css/util.css';
 
 export default () => (
   <Layout>
-    <h1>Cliente, publique sua demanda.</h1>
-    <p>Aqui, o cliente poderá criar sua demanda.</p>
+    {/*<h1> Nova Demanda</h1>
+    <p>Solicite um novo serviço que esteja precisando!</p>
+
+    <div> 
+      <form>
+        <label> Name: <input type="text" name="tittle" /></label>
+        <label> Tipo de serviço: <input type="text" name="service_type" /></label>
+        {/*<input type="submit" value="Submit" />}
+      </form>
+
+    </div>*/}
+
+    <body>
+
+    <div className="container-contact100">
+      <div className="wrap-contact100">
+        <form className="contact100-form validate-form">
+          <span className="contact100-form-title">
+            Nova Demanda
+          </span>
+
+          <div className="wrap-input100 validate-input" data-validate="Name is required">
+            <span className="label-input100">Título</span>
+            <input className="input100" type="text" name="name" placeholder="Idenfique da melhor forma a sua demanda"/>
+            <span className="focus-input100"></span>
+          </div>
+
+        
+          <div className="wrap-input100 input100-select">
+            <span className="label-input100">Tipo de demanda</span>
+            <div>
+              <select className="selection-2" name="service">
+                <option>Escolher</option>
+                <option>Online Store</option>
+                <option>eCommerce Bussiness</option>
+                <option>UI/UX Design</option>
+                <option>Online Services</option>
+              </select>
+            </div>
+            <span className="focus-input100"></span>
+          </div>
+
+          <div className="wrap-input100 validate-input" data-validate = "Message is required">
+            <span className="label-input100">Descrição</span>
+            <textarea className="input100" name="message" placeholder="Descreva as necessidades da sua demanda"></textarea>
+            <span className="focus-input100"></span>
+          </div>
+    
+          <div className="wrap-input100 validate-input" data-validate="Name is required">
+            <span className="label-input100">Prazo limite (opcional)</span>
+            <input className="input100" type="text" name="name" placeholder="Data máxima para ter o trabalho entregue"/>
+            <span className="focus-input100"></span>
+          </div>
+
+          <div className="container-contact100-form-btn">
+            <div className="wrap-contact100-form-btn">
+              <div className="contact100-form-bgbtn"></div>
+              <button className="contact100-form-btn">
+                <span>
+                  Enviar
+                  <i className="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
+                </span>
+              </button>
+            </div>
+          </div>
+
+          
+        </form>
+      </div>
+    </div>
+    </body>
+
   </Layout>
+  
 );
