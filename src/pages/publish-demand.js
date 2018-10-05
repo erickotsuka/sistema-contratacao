f031349cea54cb86c2c683644c12cb08136036a3
Lucas Magalhães <lucas.magalhaes.cordeiro@usp.br>
2018-10-05 09:55:53 -0300

    Alteracao nos campos de formulario e adicao de paddings

--- src/pages/publish-demand.js
+++ src/pages/publish-demand.js
@@ -1,58 +1,78 @@
 import React from 'react'
 import Layout from '../components/layout'
 import TwoSteps from '../components/step'
+import DateTimeFormInline from '../components/date_input'
 import { Container, Divider } from 'semantic-ui-react'
 import { Form, Input, TextArea, Button } from 'semantic-ui-react'
 import { Dropdown } from 'semantic-ui-react'
+import {
+  DateInput,
+  DatesRangeInput
+} from 'semantic-ui-calendar-react';
+
 
 export default () => (
   <Layout>
     <Container textAlign="center">
-      <div>
+      <div style={{ paddingTop: 50}}>
         <h1>Nova Demanda</h1>
       </div>
 
-      <TwoSteps />
+      <div style={{ paddingBottom: 40 , paddingTop: 40}}>
+        <TwoSteps />
+      </div>
+      
     </Container>
     <Container>
       <div>
         <Form>
-          <Form.Field
-            className
-            id="form-input-control-title"
-            control={Input}
-            label="Título"
-            placeholder="Escreva um título que melhor identifique sua demanda"
-          />
-
-          <Form.Field label="Tipo de serviço"/>
-          <Dropdown
-          
-              placeholder="Selecione um serviço"
-              fluid
-              selection
-              options={[
-                {
-                  text: 'Arquitetônico',
-                  text: 'Análise de Negócio',
-                  text: 'Programação',
-                  text: 'Tradutor',
-                },
-              ]}
+          <div style={{ paddingBottom: 20 }}>
+            <Form.Field 
+              style={{ paddingBottom: 10 }}
+              className
+              id="form-input-control-title"
+              control={Input}
+              label="Título"
+              placeholder="Escreva um título que melhor identifique sua demanda"
             />
+          </div>
 
-          <Form.Field
-            id="form-textarea-control-description"
-            control={TextArea}
-            label="Descrição"
-            placeholder="Descreva sua demanda aqui"
-          />
-          <Form.Field
-            id="form-button-control-public"
-            control={Button}
-            content="Enviar"
-            label=""
-          />
+          <div style={{ paddingBottom: 20 }}>
+            <Form.Field label="Tipo de serviço"/>
+            <Dropdown
+                placeholder="Selecione um serviço"
+                fluid
+                selection
+                options={[
+                  { text: 'Arquitetônico', value: 'Arquitetônico'},
+                  { text: 'Análise de Negócio', value: 'Análise de Negócio'},
+                  { text: 'Programação', value: 'Programação'},
+                  { text: 'Tradutor', value: 'Tradutor'}
+                ]}
+              />
+          </div>
+                
+          <div style={{ paddingBottom: 20 }}>
+            <Form.Field style={{ paddingBottom: 0 }}
+              id="form-textarea-control-description"
+              control={TextArea}
+              label="Descrição"
+              placeholder="Descreva sua demanda aqui"
+            />
+          </div>
+          
+
+               
+
+
+          <Container textAlign="right">
+            <Form.Field 
+              id="form-button-control-public"
+              control={Button}
+              content="Enviar"
+              label=""
+            />
+          </Container>
         </Form>
       </div>
     </Container>
