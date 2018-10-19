60e19d223deed763502ebd2037eb184836ae12cb
João Victor Oliveira <joao.marques.oliveira@usp.br>
2018-10-19 15:19:41 -0300

    tentativa de fazer paginas de aceite e recusa de proposta

--- src/pages/analyse-proposal.js
+++ src/pages/analyse-proposal.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Layout from '../components/layout';
+import { Router, Route, withRouter } from 'react-router';
+
 import {
   Container,
   Button,
@@ -29,69 +31,121 @@ let servico = {
   descricao: 'Descrição Lorem Ipsum. Non ut amet irure ipsum ut excepteur eu sit. Deserunt ea pariatur amet fugiat non. In incididunt nostrud aliquip consequat tempor minim in duis excepteur Lorem et in amet sit. Amet aliquip elit ullamco eu. Mollit esse ex anim reprehenderit aute ut cillum elit aliquip. Duis velit incididunt sunt laborum sit nulla cupidatat qui. Eu officia dolor sint Lorem pariatur adipisicing tempor adipisicing in.',
   entregas: ['Fazer as telas', 'Fazer o tema', 'foo bar'],
   prazo: { inicio: new Date('01/01/2018'), fim: new Date('01/01/2019') },
-  equipe: [ new IntegranteEquipe('Erick', 'desenvolvedor', 'https://react.semantic-ui.com/images/avatar/small/matthew.png'),
-            new IntegranteEquipe('João', 'desenvolvedor', 'https://react.semantic-ui.com/images/avatar/small/matthew.png'),
-            new IntegranteEquipe('Lucas', 'desenvolvedor', 'https://react.semantic-ui.com/images/avatar/small/matthew.png')],
+  equipe: [new IntegranteEquipe('Erick', 'desenvolvedor', 'https://react.semantic-ui.com/images/avatar/small/matthew.png'),
+  new IntegranteEquipe('João', 'desenvolvedor', 'https://react.semantic-ui.com/images/avatar/small/matthew.png'),
+  new IntegranteEquipe('Lucas', 'desenvolvedor', 'https://react.semantic-ui.com/images/avatar/small/matthew.png')],
   aceito: false
 }
 
-export default class AnalyseProposalPage extends Component {
+class AnalyseProposalPage extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      screen: 'show-proposal'
+    }
+  }
+
+  changePage(page) {
+    this.setState({ screen: page })
+  }
+
   render() {
     return (
-      <Layout>
+      <div>
         {
-          <Container textAlign='justified'>
-            <div>
-              <Header as='h1'>{servico.nome}</Header>
-              <Message textAlign='' >{servico.descricao}</Message>
-            </div>
-            <Divider section></Divider>
-            <div>
-              <Header as='h2'>Entregas</Header>
-              <List relaxed>
-                { 
-                  servico.entregas.map(function(descricaoEntrega, index) {
-                  return (
-                    <List.Item>
-                      <List.Content>Entrega nº {index + 1} : {descricaoEntrega}</List.Content>
-                    </List.Item>
-                  );
-                })}
-              </List>
-            </div>
-            <Divider section></Divider>
-            <div>
-              <Header as='h2'>Prazo</Header>
-              <p>Início: {servico.prazo.inicio.toLocaleDateString()} Fim: {servico.prazo.fim.toLocaleDateString()}</p>
-            </div>
-            <Divider section></Divider>
-            <div>
-              <Header as='h2'>Equipe</Header>
-              <List>
-                {
-                  servico.equipe.map(function(integrante){
-                    return (
-                      <List.Item>
-                      <Image avatar src={integrante.avatar} />
-                      <List.Content>
-                        <List.Header as='a'>{integrante.nome}</List.Header>
-                        <List.Content>{integrante.funcao}</List.Content>
-                      </List.Content>
-                      </List.Item>
-                    );
-                  })
-                }
-              </List>
-            </div>
-            <br />
-            <br />
-            <div>
-              <Button floated='left'  >Não aceitar</Button>
-              <Button positive floated='right' >Aceitar e Contratar</Button>
-            </div>
-          </Container>
+          this.state.screen === 'show-proposal' ?
+            <Layout>
+              {
+                <Container textAlign='justified'>
+                  <div>
+                    <Header as='h1'>{servico.nome}</Header>
+                    <Message textAlign='' >{servico.descricao}</Message>
+                  </div>
+                  <Divider section></Divider>
+                  <div>
+                    <Header as='h2'>Entregas</Header>
+                    <List relaxed>
+                      {
+                        servico.entregas.map(function (descricaoEntrega, index) {
+                          return (
+                            <List.Item>
+                              <List.Content>Entrega nº {index + 1} : {descricaoEntrega}</List.Content>
+                            </List.Item>
+                          );
+                        })}
+                    </List>
+                  </div>
+                  <Divider section></Divider>
+                  <div>
+                    <Header as='h2'>Prazo</Header>
+                    <p>Início: {servico.prazo.inicio.toLocaleDateString()} Fim: {servico.prazo.fim.toLocaleDateString()}</p>
+                  </div>
+                  <Divider section></Divider>
+                  <div>
+                    <Header as='h2'>Equipe</Header>
+                    <List>
+                      {
+                        servico.equipe.map(function (integrante) {
+                          return (
+                            <List.Item>
+                              <Image avatar src={integrante.avatar} />
+                              <List.Content>
+                                <List.Header as='a'>{integrante.nome}</List.Header>
+                                <List.Content>{integrante.funcao}</List.Content>
+                              </List.Content>
+                            </List.Item>
+                          );
+                        })
+                      }
+                    </List>
+                  </div>
+                  <br />
+                  <br />
+                  <div>
+                    <Button floated='left' onClick={this.changePage('refuse-proposal')}>Não aceitar</Button>
+                    <Button positive floated='right' onClick={this.changePage('accept-proposal')}>Aceitar e Contratar</Button>
+                  </div>
+                </Container>
+              }
+            </Layout>
+          : this.state.screen === 'accept-proposal' ?
+            <Layout>
+              {
+                <Container textAlign='justified'>
+                  <div>
+                    <Header as='h1'>Aceito</Header>
+                    <Message textAlign='' >foo bar</Message>
+                  </div>
+                </Container>
+              }
+            </Layout>
+          : this.state.screen === 'refuse-proposal' ?
+            <Layout>
+              {
+                <Container textAlign='justified'>
+                  <div>
+                    <Header as='h1'>Não Aceito</Header>
+                    <Message textAlign='' >foo bar baz</Message>
+                  </div>
+                </Container>
+              }
+            </Layout>
+            : // não sei o que aconteceu pra chegar aqui
+            <Layout>
+              {
+                <Container textAlign='justified'>
+                  <div>
+                    <Header as='h1'>Deu ruim</Header>
+                    <Message textAlign='' >foo bar baz</Message>
+                  </div>
+                </Container>
+              }
+            </Layout>
         }
-      </Layout>
+      </div>
     )
   }
 }
+
+export default AnalyseProposalPage
