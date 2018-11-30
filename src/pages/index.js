e7c45bb25f3019d53e7f7f036511da74108982d3
Lucas Magalhães <lucas.magalhaes.cordeiro@usp.br>
2018-11-30 17:29:27 -0200

    :art: Design changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,30 @@
 import React, { Component } from 'react'
+import { Link } from 'gatsby'
 import { Grid, Container, Image} from 'semantic-ui-react'
 import Layout from '../components/layout'
-//import {Image, Container} from 'semantic-ui-react'
+
 
 class IndexPage extends Component{
   render() {
     return(
     /*Site name on: src/components/layout->header*/
     <div style={{ background: '#FFFFFF', padding: "0px 0px"}}>
-      <Layout>
-        <Grid columns={2}>
-            <Grid.Row>
-              <Grid.Column textAlign='left'>
-              <Image src='https://www.ringtonez.app/amp/ensof/home.png' size='big'/>
-              </Grid.Column>
-              <Grid.Column textAlign='left'>
-                <Image src='https://www.ringtonez.app/amp/ensof/pr.png' size='big'/>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-      </Layout>
+      <Layout/>
+      
       <div style={{ background: '#444444' }}>
-            <Image src='https://www.ringtonez.app/amp/ensof/home5.png' fluid centered/>
-          </div>
+        <Image src='https://www.ringtonez.app/amp/ensof/home5.png' fluid centered/>
+      </div>
       <div style={{ background: '#444444' }}>
-        <Container textAlign='left'>
-          <Image src='https://www.ringtonez.app/amp/ensof/pr.png' size='large' centered/>
-        </Container>
+        <Grid columns={2} verticalAlign='middle'>
+          <Grid.Column textAlign='left' verticalAlign='middle'>
+            <h1 style={{ padding: '0px 50px' }}>
+              <Link style={{ color: 'white' }} to="/">{"Oi Kaique"}</Link>
+            </h1>
+          </Grid.Column>
+          <Grid.Column verticalAlign='middle'>
+            <Image src='https://www.ringtonez.app/amp/ensof/pr.png' size='large' centered/>
+          </Grid.Column>
+        </Grid>
       </div>
     </div>
 
